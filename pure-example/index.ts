a38a4885f085a965719c4cab7adce8187d01598d
stacktracer <vostroknutov@hotmail.com>
2018-12-05 15:52:22 +0300

    +

---
+++ pure-example/index.ts
@@ -0,0 +1,24 @@
+import { AnyAction, applyMiddleware, compose, createStore } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { filter, mapTo } from 'rxjs/operators';
+
+((win: any) => {
+
+    const rootEpic = action$ => action$.pipe(
+        filter((action: AnyAction) => action.type === 'PING'),
+        mapTo({ type: 'PONG' })
+      );
+    let id = 0;
+    win.testReduxCreateStore = () => {
+        const composeEnhancers = win.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && win.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({name:`store${id++}`})  || compose;
+        const epicMiddleware = createEpicMiddleware();
+        const store = createStore(
+            (state, action) => state,
+            composeEnhancers(
+                applyMiddleware(epicMiddleware)
+            )
+        )
+        epicMiddleware.run(rootEpic);
+    }
+
+})(window);
